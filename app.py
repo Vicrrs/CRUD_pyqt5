d358bdc7e1de409be5c4c19ff992461fe65f70d7
VICTOR ROZA SOUZA <enricofermi2019@gmail.com>
2022-11-26 00:45:45 -0400

    funca para inserir dados no db sqlite3

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ import sqlite3
 
 # criando conexao com db
 conn = sqlite3.connect('minha_lista.db')
+# cirando cursor
+c = conn.cursor()
+
+# criando tabela
+c.execute("""CREATE TABLE if not exists todo_list(
+    list_item text)
+    """)
+
+# commitando as mudanças
+conn.commit()
+
+# fechar conexão
+conn.close()
 
 
 class App(QMainWindow, Ui_MainWindow):
@@ -16,6 +29,8 @@ class App(QMainWindow, Ui_MainWindow):
         self.deletando_item_qpushbutton.clicked.connect(self.delete_item)
         self.limparlista_qpushbutton.clicked.connect(self.clear_item)
         self.salvadb_pushButton.clicked.connect(self.save_item)
+        # pega todos os itens do banco de dados
+        self.grab_all()
 
     def add_item(self):
         # pegue o item da caixa de listagem
@@ -35,14 +50,52 @@ class App(QMainWindow, Ui_MainWindow):
         self.minhaLista_listWidget.clear()
 
     def save_item(self):
-        # criar dicionário em branco para armazenar itens de tarefas
+        # criando conexao com db
+        conn = sqlite3.connect('minha_lista.db')
+        # cirando cursor
+        c = conn.cursor()
+
+        # excluindo tudo na tabela do banco de dados
+        c.execute('DELETE FROM todo_list;',)
+
+        # criar lista em branco para armazenar itens de tarefas
         items = []
         # percorrer a lista e retirar cada item
         for index in range(self.minhaLista_listWidget.count()):
             items.append(self.minhaLista_listWidget.item(index))
 
         for item in items:
-            print(item.text())
+            # print(item.text())
+            # adicionando as coisas na tabela
+            c.execute("INSERT INTO todo_list VALUES (:item)",
+                      {
+                          'item': item.text(),
+                      })
+
+            # commitando as mudanças
+        conn.commit()
+
+        # fechar conexão
+        conn.close()
+
+    def grab_all(self):
+        # criando conexao com db
+        conn = sqlite3.connect('minha_lista.db')
+        # cirando cursor
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM todo_list")
+        records = c.fetchall()
+
+        # commitando as mudanças
+        conn.commit()
+
+        # fechar conexão
+        conn.close()
+
+        # fazer loop através de registros e adicionar à tela
+        for record in records:
+            self.minhaLista_listWidget.addItem(str(record))
 
 
 if __name__ == '__main__':
