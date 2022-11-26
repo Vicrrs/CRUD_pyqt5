a850febd773d5f1f74d71ae836b457eba6862081
VICTOR ROZA SOUZA <enricofermi2019@gmail.com>
2022-11-25 22:54:00 -0400

    funcao adiciona item

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import sys
-from janela import Ui_MainWindow
-from PyQt5.QtWidgets import QMainWindow, QApplication
-
+from janela import *
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt5.QtGui import QPixmap
 
 class App(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
+        super().setupUi(self)
+        self.adicionar_item_qpushbutton.clicked.connect(self.add_item)
+        # self.adicionando_item_qlineEdit.clicked.connect(self.add_item)
+        self.deletando_item_qpushbutton.clicked.connect(self.delete_item)
+        self.limparlista_qpushbutton.clicked.connect(self.clear_item)
 
     def add_item(self):
-        pass
-
+        # pegue o item da caixa de listagem
+        item = self.adicionando_item_qlineEdit.text()
+        # adicionando item na lista
+        self.minhaLista_listWidget.addItem(item)
+        # limpando a caixa
+        self.adicionando_item_qlineEdit.setText("")
 
     def delete_item(self):
         pass
-
 
     def clear_item(self):
         pass
